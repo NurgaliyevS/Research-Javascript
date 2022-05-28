8c80730c65ad7fe974323daf9d5351e96174cb45
Sabyr Nurgaliyev <snurgaliev@cic.kz>
2022-05-28 11:17:19 +0600

    Prefix vs Postfix increment

--- index.js
+++ index.js
@@ -16,4 +16,16 @@ const lenArray = mapForEach(strArray, function(item){
   return item.length;
 })
 
-console.log(lenArray)
+// console.log(lenArray)
+
+let i = 0;
+
+// Prefix increment (++i) increments, and returns the new, incremented value;
+console.log(++i, 'Prefix increment'); // result is 1
+console.log(i); // result 1
+
+let b = 0;
+
+// Postfix increment (i++) increments, but returns the old value (i.e. the value before the increment).
+console.log(b++, 'Postfix increment'); //result 0
+console.log(b) // result 1
